1eec760e70f45d5b7b8e4601ef8b57a44abbe88a
mmahlies <eng.m.mahlies@gmail.com>
2021-10-06 00:24:27 +0200

    DS from robert sedwirg BST

--- Week5_6/RobertSedwergTest/UnitTest1.cs
+++ Week5_6/RobertSedwergTest/UnitTest1.cs
@@ -20,7 +20,193 @@ namespace RobertSedwergTest
             tree.Put(2, 1);
 
             Assert.AreEqual(tree.Get(2).Value,1) ;
-            Assert.AreEqual(tree.Get(4).N, 2);
+            Assert.AreEqual(tree.Get(4).RankNo, 2);
         }
+
+        [Test]
+        public void Min()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+
+            Assert.AreEqual(tree.Min(tree.Root).Key, 2);
+            
+        }
+
+        [Test]
+        public void Max()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+
+            Assert.AreEqual(tree.Max(tree.Root).Key, 6);
+
+        }
+
+
+
+        [Test]
+        public void Floor()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+
+            Assert.AreEqual(tree.Floor(6).Key, 6);
+            Assert.AreEqual(tree.Floor(3).Key, 2);
+
+        }
+
+
+        [Test]
+        public void Ceiling()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            Assert.AreEqual(tree.Ceiling(6).Key, 6);
+            Assert.AreEqual(tree.Ceiling(3).Key, 4);
+
+        }
+
+        [Test]
+        public void Rank()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            Assert.AreEqual(tree.Rank(6), 3);
+            Assert.AreEqual(tree.Rank(3), 1);
+
+            Assert.AreEqual(tree.RankWithoutInternalFeild(6), 3);
+            Assert.AreEqual(tree.RankWithoutInternalFeild(3), 1);
+
+
+        }
+
+
+        [Test]
+        public void Select()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            Assert.AreEqual(tree.Select(2).Key, 5);
+            Assert.AreEqual(tree.Select(3).Key, 6);
+
+        }
+
+
+        [Test]
+        public void Keys()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            var q = tree.Keys();
+            var n = q.Count;
+                
+                Assert.AreEqual(q.Dequeue().Key, 2);
+                Assert.AreEqual(q.Dequeue().Key, 4);
+                Assert.AreEqual(q.Dequeue().Key, 5);
+                Assert.AreEqual(q.Dequeue().Key, 6);
+            
+            
+            
+            
+
+        }
+        [Test]
+        public void RangeKeys()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            tree.Put(1, 4);
+            tree.Put(7, 4);
+            tree.Put(8, 4);
+
+
+            var q = tree.Keys(4,7);
+            var n = q.Count;
+
+            Assert.AreEqual(q.Dequeue().Key, 4);
+            Assert.AreEqual(q.Dequeue().Key, 5);
+            Assert.AreEqual(q.Dequeue().Key, 6);
+            Assert.AreEqual(q.Dequeue().Key, 7);
+        }
+
+        [Test]
+        public void Delete ()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            tree.Put(4, 2);
+            tree.Put(6, 3);
+            tree.Put(2, 4);
+            tree.Put(1, 4);
+            tree.Put(7, 4);
+            tree.Put(8, 4);
+
+
+            var q = tree.Keys();
+            Assert.AreEqual(q.Count, 7);
+            tree.Delete(7);
+            tree.Delete(6);
+            q = tree.Keys();
+            Assert.AreEqual(q.Count, 5);
+
+            tree.Delete(6);
+            q = tree.Keys();
+            Assert.AreEqual(q.Count, 5);
+
+            tree.Delete(4);
+            q = tree.Keys();
+            Assert.AreEqual(q.Count, 4);
+
+        }
+
+
+        [Test]
+        public void Hight()
+        {
+            Tree tree = new Tree();
+            tree.Put(5, 1);
+            Assert.AreEqual(tree.Height(), 1);
+
+            tree.Put(4  , 2);
+            Assert.AreEqual(tree.Height(), 2);
+            tree.Put(6, 3);
+            Assert.AreEqual(tree.Height(), 2);
+            tree.Put(2, 4);                     
+            Assert.AreEqual(tree.Height(), 3);
+
+            tree.Put(3, 3);
+            Assert.AreEqual(tree.Height(), 4);
+
+            tree.Put(1, 1);
+            Assert.AreEqual(tree.Height(), 4);
+
+            Assert.AreEqual(tree.Root.HeightNo, 4);
+        }
+
     }
 }
--- Week5_6/TreeRobertSedwarg/Tree.cs
+++ Week5_6/TreeRobertSedwarg/Tree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,23 +23,25 @@ namespace TreeRobertSedwarg
 
             if (root.Key > key)
             {
-             root.Left =    Put(root.Left, key, value);
+                root.Left = Put(root.Left, key, value);
             }
             else if (root.Key < key)
             {
-             root.Rite =    Put(root.Rite, key, value);
+                root.Right = Put(root.Right, key, value);
             }
             else
             {
                 root.Value = value;
             }
-            root.N = Size(root.Left) + Size(root.Rite) + 1;
+            // to maintain the count of each node
+            root.RankNo = Size(root.Left) + Size(root.Right) + 1;
+            root.HeightNo = Math.Max(GetHeight(root.Left), GetHeight(root.Right)) + 1;
             return root;
         }
 
         public Node Get(int key)
         {
-           return Get(Root, key);
+            return Get(Root, key);
         }
 
         private Node Get(Node root, int key)
@@ -48,28 +51,322 @@ namespace TreeRobertSedwarg
 
             if (root.Key > key)
             {
-               return Get    (root.Left, key);
+                return Get(root.Left, key);
             }
             else if (root.Key < key)
             {
-                return Get(root.Rite, key);
+                return Get(root.Right, key);
             }
             else
             {
                 return root;
             }
-           
+
         }
 
+        // to get the siiiiiiiiiiiiiize oif given node
         private int Size(Node node)
         {
             if (node == null)
                 return 0;
             else
             {
-                return node.N;
+                return node.RankNo;
+            }
+        }
+
+
+        public Node Min(Node node)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+            return node.Left == null ? node : Min(node.Left);
+        }
+
+        public Node Max(Node node)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+            return node.Right == null ? node : Max(node.Right);
+        }
+
+        public Node Floor(int key)
+        {
+            return Floor(Root, key);
+        }
+
+
+        // smalest key <= to the key
+        private Node Floor(Node root, int key)
+        {
+            // case 1
+            if (root == null || root.Key == key)
+            {
+                return root;
+            }
+            // case 2
+            else if (root.Key < key)
+            {
+                // can be this root or the one one the right
+                Node t = Floor(root.Right, key);
+                if (t == null)
+                {
+                    return root;
+                }
+                else
+                {
+                    return t;
+                }
+
+
+            }
+            // case 3
+            else if (root.Key > key)
+            {
+                // go to left
+                return Floor(root.Left, key);
             }
 
+            throw new Exception("dead path");
+        }
+
+
+        public Node Ceiling(int key)
+        {
+            return Ceiling(Root, key);
+        }
+
+        private Node Ceiling(Node root, int key)
+        {
+            // case 1
+            if (root == null || root.Key == key)
+            {
+                return root;
+            }
+            // case 2
+            else if (root.Key > key)
+            {
+                // can be this root or the one one the left
+                Node t = Ceiling(root.Left, key);
+                if (t == null)
+                {
+                    return root;
+                }
+                else
+                {
+                    return t;
+                }
+
+
+            }
+            // case 3
+            else if (root.Key < key)
+            {
+                // go to left
+                return Floor(root.Right, key);
+            }
+
+            throw new Exception("dead path");
+        }
+
+        // how many keys before this key (
+        public int Rank(int key)
+        {
+            return Rank(Root, key);
+        }
+
+        private int Rank(Node root, int key)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.Key > key) 
+            {
+                return Rank(root.Left, key);
+            }
+            else if (root.Key < key)
+            {
+                return 1 + Size(root.Left) + Rank(root.Right, key);
+            }
+            else
+            {
+                return Size(root.Left);
+            }
+
+        }
+        // ex3.2.12
+        public int RankWithoutInternalFeild(int key)
+        {
+            return RankWithoutInternalFeild(Root, key);
+        }
+
+        private int RankWithoutInternalFeild(Node root, int key)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.Key > key)
+            {
+                return RankWithoutInternalFeild(root.Left, key);
+            }
+            else if (root.Key < key)
+            {
+                return 1 + RankWithoutInternalFeild(root.Left , key) + RankWithoutInternalFeild(root.Right, key);
+            }
+            else
+            {
+                return RankWithoutInternalFeild(root.Left, key) ;
+            }
+
+        }
+
+        // Return Node containing key of rank k.
+        // there are n element less than returned node
+        public Node Select(int n)
+        {
+            return Select(Root, n);
+        }
+
+        private Node Select(Node root, int n)
+        {
+            if (root == null)
+                return null;
+
+            if (Rank(root.Key) > n)
+            {
+                return Select(root.Left, n);
+            }
+            else if (Rank(root.Key) < n)
+            {
+                var t = Select(root.Right, n);
+                if (t == null)
+                    return root;
+                else
+                    return t;
+
+            }
+            else // Rank(root.Key) ==  n
+            {
+                return root;
+            }
+
+        }
+
+
+        public Queue<Node> Keys()
+        {
+            Queue<Node> q = new Queue<Node>();
+            InOrder(Root, q);
+            return q;
+        }
+
+        private void InOrder(Node root, Queue<Node> q)
+        {
+            if (root == null)
+                return;
+            InOrder(root.Left, q);
+            q.Enqueue(root);
+            InOrder(root.Right, q);
+
+
+        }
+
+        public Queue<Node> Keys(int lo, int hi)
+        {
+            Queue<Node> q = new Queue<Node>();
+            InOrder(Root, q, lo, hi);
+            return q;
+        }
+
+        private void InOrder(Node root, Queue<Node> q, int lo, int hi)
+        {
+            if (root == null)
+                return;
+            if (root.Left != null && root.Left.Key >= lo)
+                InOrder(root.Left, q, lo, hi);
+
+
+            if (root.Key >= lo && root.Key <= hi)
+                q.Enqueue(root);
+
+            if (root.Right != null && root.Right.Key <= hi)
+                InOrder(root.Right, q, lo, hi);
+
+
+
+
+        }
+
+        public void Delete(int key)
+        {
+            Root = Delete(Root, key);
+        }
+
+        private Node Delete(Node root, int key)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            if (root.Key > key)
+                root.Left = Delete(root.Left, key);
+            else if (root.Key < key)
+                root.Right = Delete(root.Right, key);
+            else // root.key == key
+            {
+
+                // case 0,1 // 0child, one child
+                if (root.Left == null) return root.Right;
+                if (root.Right == null) return root.Left;
+
+                // case 2 two child
+
+                Node successsor = Min(root.Right);
+
+                // delte min in the right
+                root.Right = Delete(root.Right, successsor.Key);
+                // relace target node with min one
+                root.Key = successsor.Key;
+                root.Value = successsor.Value;
+            }
+
+
+            // update x.count
+            root.RankNo = 1 + Size(root.Left) + Size(root.Right);
+            root.HeightNo = Math.Max(GetHeight(root.Left), GetHeight(root.Right)) + 1;
+            return root;
+
+        }
+
+
+        private int GetHeight(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                return node.HeightNo;
+            }
+        }
+        // recersive to get hight in linerar time
+        public int Height()
+        {
+            return Height(Root);
+        }
+
+        private int Height(Node root)
+        {
+            // base case
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(Height(root.Left), Height(root.Right)) + 1;
         }
     }
 
@@ -81,8 +378,10 @@ namespace TreeRobertSedwarg
         public int Value { get; set; }
 
         public Node Left { get; set; }
-        public Node Rite { get; set; }
+        public Node Right { get; set; }
 
-        public int N { get; set; }
+        public int RankNo { get; set; }
+        public int HeightNo { get; set; }
+
     }
 }
